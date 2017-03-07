2ed3f04dc7106dfc1a9004cf4df1d1302e0d5aae
skkif1 <skkif1@gmail.com>
2017-03-07 15:58:07 +0200

    need ref

--- util.c
+++ util.c
@@ -12,81 +12,16 @@
 
 #include "ft_ls.h"
 
-char *g_path;
-static int flag;
-t_list *g_temp;
-
-char *get_folder_name(char *str)
+int if_dir_present(t_list *list)
 {
-    char *temp;
+    t_item *item;
 
-    temp = g_path;
-
-    while(temp && str)
+    while (list)
     {
-        if(*temp != *str)
-        {
-            return str;
-        }
-        str++;
-        temp++;
+        item = list->content;
+        if (ft_strcmp(item->type, "d"))
+            return (1);
+        list = list->next;
     }
-    return 0;
-}
-
-char *get_root(char *str)
-{
-    char *temp;
-    int i;
-
-    temp = str;
-    i = 0;
-    while(*temp)
-    {
-        i++;
-        temp++;
-    }
-    while(*temp != '/' && i > 0)
-    {
-        temp--;
-        i--;
-    }
-    return temp;
-}
-
-static void del_item(t_item* item)
-{
-    free(item->g_name);
-    free(item->o_name);
-    free(item->name);
-    free(item->perm);
-    free(item->time);
-    free(item->type);
-    free(item->path);
-    free(item->x_atr);
-    free(item);
-}
-
-void free_folder(void* f_folder, size_t size)
-{
-    t_folder *folder;
-    t_list *item_l;
-
-    folder = f_folder;
-    item_l = folder->item_list;
-    free(folder->folder);
-    free(folder->path);
-    while(item_l)
-    {
-        del_item(item_l->content);
-        item_l = item_l->next;
-    }
-      free(folder);
-}
-
-void get_header(t_list *g_list)
-{
-    if (!flag)
-        g_temp = g_list;
-    flag = 1;
+    return (0);
 }
